a5253bc910d32b0684030efda7a9ecb840523a99
DeeBest <simphiwedladla8@gmail.com>
2024-05-01 10:50:22 +0200

    added sound files and added them to the html and javascript

--- index.js
+++ index.js
@@ -8,28 +8,30 @@ const kickSoundBtn = document.getElementById('kick');
 const snareSoundBtn = document.getElementById('snare');
 const tomSoundBtn = document.getElementById('tom');
 
+const crashSound = document.getElementById('crash-sound');
+const kickSound = document.getElementById('kick-sound');
+const snareSound = document.getElementById('snare-sound');
+const tomSound = document.getElementById('tom-sound');
+
 function playCrashSound() {
-  console.log('Crash Sound Button Clicked');
+  crashSound.play();
 }
 
 function playKickSound() {
-  console.log('Kick Sound Button Clicked');
+  kickSound.play();
 }
 
 function playSnareSound() {
-  console.log('Snare Sound Button Clicked');
+  snareSound.play();
 }
 
 function playTomSound() {
-  console.log('Tom Sound Button Clicked');
+  tomSound.play();
 }
 
 crashSoundBtn.addEventListener('click', playCrashSound);
-
 kickSoundBtn.addEventListener('click', playKickSound);
-
 snareSoundBtn.addEventListener('click', playSnareSound);
-
 tomSoundBtn.addEventListener('click', playTomSound);
 
 window.addEventListener('keydown', (e) => {
