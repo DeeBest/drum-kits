5463023b93a9206e5717c55a039923e11835ba97
DeeBest <simphiwedladla8@gmail.com>
2024-05-01 09:33:32 +0200

    imported the sound buttons into javascript and added event listeners for each.

--- index.js
+++ index.js
@@ -2,3 +2,24 @@ const currentYear = new Date().getFullYear();
 
 const footerParagraph = document.getElementById('footer-paragraph');
 footerParagraph.textContent = `Created By \xA9Simphiwe ${currentYear}.`;
+
+const crashSoundBtn = document.getElementById('crash');
+const kickSoundBtn = document.getElementById('kick');
+const snareSoundBtn = document.getElementById('snare');
+const tomSoundBtn = document.getElementById('tom');
+
+crashSoundBtn.addEventListener('click', () => {
+  console.log('Sound Button Clicked');
+});
+
+kickSoundBtnSoundBtn.addEventListener('click', () => {
+  console.log('Sound Button Clicked');
+});
+
+snareSoundBtn.addEventListener('click', () => {
+  console.log('Sound Button Clicked');
+});
+
+tomSoundBtnSoundBtn.addEventListener('click', () => {
+  console.log('Sound Button Clicked');
+});
